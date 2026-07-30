8c4341d9da6358162545d30f7769540b5a114745
Nikitha Tom <nikithatom05@gmail.com>
2026-07-30 06:57:33 +0000

    Import Blender lab model and update 3D scene

--- frontend/src/scenes/LabScene.jsx
+++ frontend/src/scenes/LabScene.jsx
@@ -1,36 +1,19 @@
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, useGLTF } from "@react-three/drei";
 
-function Cube() {
-  return (
-    <mesh position={[0, 1, 0]}>
-      <boxGeometry args={[2, 2, 2]} />
-      <meshStandardMaterial color="royalblue" />
-    </mesh>
-  );
+function LabModel() {
+  const { scene } = useGLTF("/models/computer_lab.glb");
+
+  return <primitive object={scene} scale={1} />;
 }
 
-function Floor() {
+export default function LabScene() {
   return (
-    <mesh rotation={[-Math.PI / 2, 0, 0]}>
-      <planeGeometry args={[20, 20]} />
-      <meshStandardMaterial color="#cccccc" />
-    </mesh>
-  );
-}
-
-function LabScene() {
-  return (
-    <Canvas camera={{ position: [6, 5, 6], fov: 50 }}>
+    <Canvas camera={{ position: [10, 8, 10], fov: 50 }}>
       <ambientLight intensity={1} />
       <directionalLight position={[5, 10, 5]} />
-
-      <Cube />
-      <Floor />
-
+      <LabModel />
       <OrbitControls />
     </Canvas>
   );
 }
-
-export default LabScene;
